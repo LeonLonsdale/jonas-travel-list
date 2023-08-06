88e4fd91cfee621c0ec5630adf2be6bc4b34d316
LeonLonsdale <leon.dev@icloud.com>
2023-08-06 12:31:57 +0100

    add state to controlled elements in Form component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 const initialItems = [
   {id: 1, description: 'Passports', quantity: 2, packed: false},
   {id: 2, description: 'Socks', quantity: 12, packed: true},
@@ -18,6 +20,9 @@ const Logo = () => {
   return <h1>🛫🌴 Far Away 🧳🌞</h1>;
 };
 const Form = () => {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -25,14 +30,19 @@ const Form = () => {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select name="" id="">
+      <select name="" id="" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({length: 20}, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
