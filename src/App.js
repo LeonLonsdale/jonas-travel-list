441e984533e19f8292a39a5e5815c1536d10e5e5
LeonLonsdale <leon.dev@icloud.com>
2023-08-06 12:03:49 +0100

    basic item list setup and form setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  {id: 1, description: 'Passports', quantity: 2, packed: false},
+  {id: 2, description: 'Socks', quantity: 12, packed: true},
+];
+
 const App = () => {
   return (
     <div className="app">
@@ -13,15 +18,48 @@ const Logo = () => {
   return <h1>🛫🌴 Far Away 🧳🌞</h1>;
 };
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select name="" id="">
+        {Array.from({length: 20}, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 };
 const PackingList = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+const Item = ({item}) => {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
 const Stats = () => {
   return (
     <footer className="stats">
