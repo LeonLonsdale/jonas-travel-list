6526dac03eb67f98fbf07d4a67d5057b02a02549
LeonLonsdale <leon.dev@icloud.com>
2023-08-09 17:37:37 +0100

    conditional footer displays

--- src/App.js
+++ src/App.js
@@ -87,14 +87,22 @@ const Item = ({item, onDelItems, onToggleItems}) => {
 };
 
 const Stats = ({items}) => {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list</em>
+      </footer>
+    );
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed === true).length;
-  const packedPerc = Math.round((numPacked / numItems) * 100);
+  const packedPerc = Math.round((numPacked / numItems) * 100) || 0;
   return (
     <footer className="stats">
       <em>
-        You have {numItems} items in your list. You've already packed {numPacked} ({packedPerc}%) of
-        those items.
+        {packedPerc === 100
+          ? `You've got everything  🛫`
+          : `You have ${numItems} items in your list. You've already packed ${numPacked} (${packedPerc}%) of
+        those items.`}
       </em>
     </footer>
   );
